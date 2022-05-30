93637ebc6db7afcc673c4dd4b8fbe2f6c8cee297
Yograj Dak <104014739+yograj0344@users.noreply.github.com>
2022-05-30 14:27:39 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,16 @@
+class factorial
+{
+    public static void main(String[] args)
+    {
+        int n=5;
+        int fact=1;
+
+        for(int i=2; i<=n; i++)
+        {
+            fact=fact*i;
+            System.out.println(fact);
+
+        }
+
+    }
+}
